52b7468ea8ce3a0a0d62fbe339a6ffa163a3855c
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-04-22 14:07:40 +0000

    added some more functionalities

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const fastify = require("fastify");
 const fmongo = require("fastify-mongodb");
 const app = fastify();
-
+const data = require("./data.json")
 
 // TODO: Connect to mongoDB
-app.register(fmongo, {
-  forceClose: true,
-  url: 'mongodb://mongo/mydb'
-})
-
+// app.register(fmongo, {
+//   forceClose: true,
+//   url: 'mongodb://mongo/mydb'
+// })
 
 
 app.get("/",(req,res)=>{
-    res.send({name:"Tushar Rajpoot"});
+    res.send(data);
 })
 
+app.get("/donut",(req,res)=>{
+    res.send(data[0])
+})
 
-
-
-
+app.get("/donut/topping",(req,res)=>{
+    res.send(data[0].topping)
+})
 
 
 app.listen(3500,()=>{
